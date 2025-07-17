aeeb1a9a4e2489d13de43deb7f31422646bd85f9
Jason Hobbs <jason.hobbs@gmail.com>
2025-07-17 06:28:59 -0500

    Convert dilution demo to TS

--- demos-framework/src/main.ts
+++ demos-framework/src/main.ts
@@ -13,6 +13,7 @@ const demoRegistry: Record<string, () => Promise<{ default: (container: HTMLElem
   'diagonalization': () => import('@demos/real-analysis/topology/diagonalization'),
   'turntable': () => import('@demos/differential-equations/turntable'),
   'pendulum': () => import('@demos/differential-equations/pendulum'),
+  'dilution-calculator': () => import('@demos/differential-equations/dilution-calculator'),
   'cellular-automata/game-of-life': () => import('@demos/cellular-automata/game-of-life'),
   'cellular-automata/elementary-cellular-automata': () => import('@demos/cellular-automata/elementary-cellular-automata')
 };
