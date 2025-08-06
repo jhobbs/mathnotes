4606930939186f8f5e506666230895e3e773275e
Jason Hobbs <jason.hobbs@gmail.com>
2025-08-06 10:51:58 -0500

    Rearrange python code

--- scripts/build_static_simple.py
+++ scripts/build_static_simple.py
@@ -11,7 +11,7 @@ import logging
 # Add parent directory to path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from generator.builder import SiteBuilder
+from mathnotes.sitegenerator.builder import SiteBuilder
 
 # Configure logging
 logging.basicConfig(
