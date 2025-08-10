bcaea4163757c60c5d32d4e2fa4bde96064b5258
Jason Hobbs <jason.hobbs@gmail.com>
2025-08-10 10:42:13 -0500

    start removing probably unused code

--- mathnotes/url_mapper.py
+++ mathnotes/url_mapper.py
@@ -5,7 +5,6 @@ URL mapping and routing utilities for the Mathnotes application.
 from pathlib import Path
 import frontmatter
 from typing import Dict
-from .utils import slugify
 from .config import CONTENT_DIRS
 
 
